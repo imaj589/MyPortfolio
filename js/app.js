f67ffc8da93638285df5f4b802255d42b85f891d
imaj589 <110421728+imaj589@users.noreply.github.com>
2022-08-02 12:29:48 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -9,13 +9,18 @@ $(document).ready(function(){
 let hamburger=document.querySelector('.hamburger');
 let times=document.querySelector('.times');
 let mobileNav=document.querySelector('.mobile-nav');
-
+let links=document.querySelector('.links');
 
 hamburger.addEventListener('click',function(){
     mobileNav.classList.add('open');
 });
 
+
+
 times.addEventListener('click',function(){
+    mobileNav.classList.remove('open');
+});
+links.addEventListener('click',function(){
     mobileNav.classList.remove('open');
 });
 
